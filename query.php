f965460999490a3ad1e4d461c58272c5de422179
Ryan Thompson <service@aiwebsystems.com>
2014-02-04 10:52:14 -0600

    Remove tabs

--- query.php
+++ query.php
@@ -5,90 +5,90 @@
  *
  * Use tags to execute MySQL queries 
  *
- * @package		Query
- * @version		1.1
- * @author		Ryan Thompson - AI Web Systems, Inc.
- * @copyright	Copyright (c) 2008 - 2014 AI Web Systems, Inc.
- * @link		https://www.aiwebsystems.com/docs/plugins/query
+ * @package        Query
+ * @version        1.1
+ * @author        Ryan Thompson - AI Web Systems, Inc.
+ * @copyright    Copyright (c) 2008 - 2014 AI Web Systems, Inc.
+ * @link        https://www.aiwebsystems.com/docs/plugins/query
  */
 class Plugin_query extends Plugin
 {
-	/**
-	 * Possible entries parameters
-	 * @var array
-	 */
-	public $get_parameters = array(
-		'select'			=> '*',
-		'table'				=> null,
-		'limit'				=> 1000,
-		'where'				=> null,
-		'order_by'			=> null,
-		'sort'				=> 'DESC',
-		);
+    /**
+     * Possible entries parameters
+     * @var array
+     */
+    public $get_parameters = array(
+        'select'            => '*',
+        'table'                => null,
+        'limit'                => 1000,
+        'where'                => null,
+        'order_by'            => null,
+        'sort'                => 'DESC',
+        );
 
-	///////////////////////////////////////////////////////////////////////////////
-	// --------------------------	METHODS 	  ------------------------------ //
-	///////////////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////////
+    // --------------------------    METHODS       ------------------------------ //
+    ///////////////////////////////////////////////////////////////////////////////
 
-	/**
-	 * Get
-	 *
-	 * {{ query:get table="comments" select="comment" }}
-	 * 		{{ field }}
-	 * {{ /query:get }}
-	 *
-	 * @access	public
-	 * @return	array
-	 */
-	public function get()
-	{
-		/**
-		 * Get Plugin Attributes
-		 */
-		
-		$parameters = array();
-		
-		foreach ($this->get_parameters as $parameter => $parameter_default)
-		{
-			$parameters[$parameter] = $this->getAttribute($parameter, $parameter_default);
-		}
+    /**
+     * Get
+     *
+     * {{ query:get table="comments" select="comment" }}
+     *         {{ field }}
+     * {{ /query:get }}
+     *
+     * @access    public
+     * @return    array
+     */
+    public function get()
+    {
+        /**
+         * Get Plugin Attributes
+         */
+        
+        $parameters = array();
+        
+        foreach ($this->get_parameters as $parameter => $parameter_default)
+        {
+            $parameters[$parameter] = $this->getAttribute($parameter, $parameter_default);
+        }
 
-		
-		/**
-		 * Process the shit out of everything
-		 */
-		
-		// Table
-		$query = ci()->pdb->table($parameters['table']);
+        
+        /**
+         * Process the shit out of everything
+         */
+        
+        // Table
+        $query = ci()->pdb->table($parameters['table']);
 
-		// Select
-		$query->select(explode('|', $parameters['select']));
+        // Select
+        $query->select(explode('|', $parameters['select']));
 
-		// Where
-		if ($parameters['where'])
-			$query->where($parameters['where']);
+        // Where
+        if ($parameters['where'])
+            $query->where($parameters['where']);
 
-		// Order by
-		if ($parameters['order_by'])
-			$query->orderBy($parameters['order_by'], $parameters['sort']);
+        // Order by
+        if ($parameters['order_by'])
+            $query->orderBy($parameters['order_by'], $parameters['sort']);
 
-		// Limit
-		if ($parameters['limit'])
-			$query->limit($parameters['limit']);
+        // Limit
+        if ($parameters['limit'])
+            $query->limit($parameters['limit']);
 
-		
-		/**
-		 * Donzo - build the return
-		 */
+        
+        /**
+         * Donzo - build the return
+         */
 
-		$return = array();
+        $return = array();
 
-		// Count all results
-		$return['total'] = $query->count();
+        // Count all results
+        $return['total'] = $query->count();
 
-		// Results
-		$return['results'] = $query->get();
+        // Results
+        $return['results'] = $query->get();
 
-		return array($return);
-	}
+        return array($return);
+    }
 }
